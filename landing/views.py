08e2114530d1bf95bdc0f154575d0b08b2d28613
gordey <gordey0511@yandex.ru>
2018-06-07 22:52:10 +0600

    4

--- landing/views.py
+++ landing/views.py
@@ -20,6 +20,6 @@ def landing(request):
 
 def home(request):
     products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
-    products_images_phones = products_images.filter(product__category__id=3)
+    products_images_phones = products_images.filter(product__category__id=1)
     products_images_laptops = products_images.filter(product__category__id=1)
     return render(request, 'landing/home.html', locals())
